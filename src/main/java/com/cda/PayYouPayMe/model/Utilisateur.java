8ca88c08b4690159d70073ae4f3f1b57bf02bde2
MaDes <madesappli@gmail.com>
2025-03-17 12:16:05 +0100

    changement sur utilisateur

--- src/main/java/com/cda/PayYouPayMe/model/Utilisateur.java
+++ src/main/java/com/cda/PayYouPayMe/model/Utilisateur.java
@@ -23,10 +23,10 @@ public class Utilisateur {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	
+	private String lastName;
+	
 	//@Column(name="Prenom")
 	private String firstName;
-	
-	private String lastName;
 	
 	private String email;
 	
